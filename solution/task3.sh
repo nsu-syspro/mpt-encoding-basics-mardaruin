72d57f45193b6c2ba2e89d42d4ccec360535a95a
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-10-26 16:40:17 +0700

    lc to 3-4 tasks added

--- solution/task3.sh
+++ solution/task3.sh
@@ -2,5 +2,6 @@
 
 # Вывести дату в формате:
 #   13 октября 2023 года, Пятница
+export LC_ALL=
 export LANG=ru_RU.UTF-8
 date +'%d %B %Y года, %A' -d "$@"
--- solution/task4.sh
+++ solution/task4.sh
@@ -2,5 +2,6 @@
 
 # Вывести дату в формате:
 #   Freitag, 13. Oktober 2023
+export LC_ALL=
 export LANG=de_DE.UTF-8
 date +'%A, %d. %B %Y' -d "$@"
